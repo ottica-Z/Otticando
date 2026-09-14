5ddbef132c8174b4078d8367664d27f992b94ee7
ottica-Z <antoniocontino01@gmail.com>
2026-09-14 20:37:18 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "otticando-v17-offline";
+const CORE = ["./", "./index.html", "./manifest.webmanifest"];
+self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())); });
+self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener("fetch", event => {
+  const req = event.request;
+  if (req.mode === "navigate") {
+    event.respondWith(fetch(req).then(res => { const copy=res.clone(); caches.open(CACHE_NAME).then(c=>c.put("./index.html",copy)); return res; }).catch(()=>caches.match("./index.html")));
+    return;
+  }
+  event.respondWith(caches.match(req).then(cached => cached || fetch(req).then(res => { const copy=res.clone(); caches.open(CACHE_NAME).then(c=>c.put(req,copy)); return res; })));
+});
